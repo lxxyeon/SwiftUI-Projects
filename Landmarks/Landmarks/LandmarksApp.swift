14ffc2b2aa4adf94477917b85e4f502d828b4f8b
lxxyeon <lxxyeon@gmail.com>
2023-07-23 15:54:14 +0900

    SwiftUI Tutorials 프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by leeyeon2 on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
